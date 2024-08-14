095a027737b4de49c99cb9eabd9390eabd019088
pablete18 <pabloealcaraz@gmail.com>
2024-08-13 23:59:19 -0300

    [feat]: Cuentas claras terminado

--- clases/LeetCode/CuentasClaras.ts
+++ clases/LeetCode/CuentasClaras.ts
@@ -2,7 +2,8 @@ class Amigo {
     public nombre : string;
     public puso : number
     public debeRecibir : number
-    public debePagar : number 
+    public debePagar : number
+     
 
     constructor(
         nombre : string,
@@ -12,6 +13,7 @@ class Amigo {
         this.puso = puso;
         this.debeRecibir = 0;
         this.debePagar = 0
+        
     }
 }
 
@@ -34,18 +36,45 @@ class CuentasClaras{
         return this.totalGastado() / this.totalAmigos.length
     }
     
-    cuentasClaras(): any {
-        this.totalAmigos.forEach(amigo=> {
-            if(amigo.puso < this.dividirTotal()){
-                amigo.debePagar = this.dividirTotal() - amigo.puso
-            }else {
-                amigo.debeRecibir = amigo.puso - this.dividirTotal()
-            }
-        }) 
+    cuentasClaras(): void{
+        const cadaAmigoPaga = this.dividirTotal()
 
+        this.totalAmigos.forEach(amigo=> {
+            if(amigo.puso < cadaAmigoPaga){
+                amigo.debePagar = cadaAmigoPaga - amigo.puso
+            }else {
+                amigo.debeRecibir = amigo.puso - cadaAmigoPaga
+            }
+        })
         
+        const amigoDeudor = this.totalAmigos.filter(amigo=> 
+            amigo.debePagar > 0        
+        )
+    
+        const amigoAcreedor = this.totalAmigos.filter(amigo=>
+            amigo.debeRecibir > 0
+           )
+
+        amigoDeudor.forEach(deudor=> {
+
+            while(deudor.debePagar > 0 && amigoAcreedor.length > 0){
+                const acreedor = amigoAcreedor[0]
+                const pago = Math.min(deudor.debePagar,acreedor.debeRecibir)
+
+                console.log(`${deudor.nombre} debe pagar ${pago} a ${acreedor.nombre}`);
+                
+                deudor.debePagar -= pago
+                acreedor.debeRecibir -= pago
+
+                if(acreedor.debeRecibir === 0){
+                    amigoAcreedor.shift()
+                }
+            }
+        })
         
     }
+
+    
 
 }
 
@@ -53,20 +82,24 @@ class CuentasClaras{
 const amigo1 = new Amigo('Pablo',2340)
 const amigo2 = new Amigo ('Rocio', 35)
 const amigo3 = new Amigo ('Rufina',23) 
+const amigo4 = new Amigo ('Monica',0)
 
 
 
-const cuentasClaras = new CuentasClaras([amigo1,amigo2,amigo3])
+const cuentasClaras = new CuentasClaras([amigo1,amigo2,amigo3,amigo4])
 
-console.log(cuentasClaras.totalGastado());
-console.log(cuentasClaras.dividirTotal());
-cuentasClaras.cuentasClaras();
+console.log(`Total gastado : ${cuentasClaras.totalGastado()}`);
+console.log(`Cada uno debe poner : ${cuentasClaras.dividirTotal()}`);
+
+ 
+
 
 
 
 
 cuentasClaras.totalAmigos.forEach(amigo=>{
-    console.log(`${amigo.nombre} debe recibir ${amigo.debeRecibir}, debe pagar : ${amigo.debePagar}`);
+    console.log(`${amigo.nombre} pago : ${amigo.puso} `);
     
 })
+cuentasClaras.cuentasClaras()
 
