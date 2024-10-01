1dcfbcba29a6d88d6d810e95af9b7ad7d086c602
pablete18 <pabloealcaraz@gmail.com>
2024-09-30 22:07:05 -0300

    string to integer in work

--- clases/LeetCode/stringToInteger.ts
+++ clases/LeetCode/stringToInteger.ts
@@ -1,3 +1,26 @@
 function myAtoi(s: string): number {
-    return  0
+    
+   const regex = /^[a-zA-Z].*/;
+   s.trim()
+   if (regex.test(s)) {
+     return 0
+   }
+   s = s.replace(/[a-zA-Z].*/, '')
+
+   const toNumber = Number(s)      
+   let numeroAbsoluto = Math.abs(toNumber)
+
+    if(numeroAbsoluto < 0){
+         numeroAbsoluto = - numeroAbsoluto
+    }
+
+    return numeroAbsoluto
+
+     /*  let powNumber = Math.pow(x,31)-1
+    let negativePow = -Math.pow(x,31)  */
+    
 };
+
+let s = "1337c0d3"
+
+console.log(myAtoi(s));
